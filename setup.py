8d93ac19d78002708003b54f51452fa48d4dd3c7
Mikhail Mezyakov <mihail265@gmail.com>
2015-03-31 11:14:43 +0300

    Fixes for Yappi and Django >= 1.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ for cmd in ('egg_info', 'develop'):
     if cmd in sys.argv:
         from setuptools import setup
 
-version='0.1'
+version='0.2'
 
 setup(
     name='django-profiling-dashboard',
@@ -16,7 +16,7 @@ setup(
 
     packages=['profiling_dashboard'],
     package_data={
-        'profiling_dashboard': ['templates/profiling_dashboard/*.html',]
+        'profiling_dashboard': ['templates/profiling_dashboard/*.html', 'static/*.css']
     },
 
     url='https://github.com/kmike/django-profiling-dashboard',
@@ -25,7 +25,7 @@ setup(
     description = """ Django profiling dashboard for debugging CPU, memory and other resources usage in live servers """,
 
     long_description = open('README.rst').read(),
-    requires = ['django (>= 1.3)', 'yappi (>= 0.54)', 'psutil (>= 0.4.1)', 'pympler (>= 0.2.1)', 'query_exchange'],
+    requires = ['django (>= 1.5)', 'yappi (>= 0.54)', 'psutil (>= 0.4.1)', 'pympler (>= 0.2.1)', 'query_exchange'],
 
     classifiers=(
         'Development Status :: 3 - Alpha',
