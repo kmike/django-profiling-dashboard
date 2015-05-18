c7d186a188003b9e44f572a306c45274b5644ef3
Mikhail Mezyakov <mihail265@gmail.com>
2015-05-18 21:03:19 +0300

    Include absolute_import; separate dir for static

--- profiling_dashboard/urls.py
+++ profiling_dashboard/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#from __future__ import absolute_import
+from __future__ import absolute_import
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('profiling_dashboard.views',
